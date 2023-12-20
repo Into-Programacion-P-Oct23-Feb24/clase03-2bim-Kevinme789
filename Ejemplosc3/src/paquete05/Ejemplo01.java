1f5fc22c696f0ed0e781aa40c5e6d43c99abd5b6
Kevin <porkevin743@gmail.com>
2023-12-20 15:35:19 -0500

    Cambios

---
+++ Ejemplosc3/src/paquete05/Ejemplo01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo01 {
+    public static void main(String[] args) {
+     int[] arreglo = new int[2];
+        for (int f = 0;f<arreglo.length;f++){
+        System.out.println (arreglo[f]);
+        }
+  
+    }
+    
+
+}
+
